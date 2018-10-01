08544724e6e442a47ac3e991a1172e7ebb188954
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-10-01 10:11:13 +0200

    Add MLE for Cauchy distribution

---
+++ src/Cauchy.R
@@ -0,0 +1,49 @@
+﻿# fixer le gemme et paramètres réels
+set.seed(2018)
+real_x0 = 13
+real_a = 0.5
+nbreaks = 20
+
+# générér échantillons
+N = 1000
+x = rcauchy(n = N,
+            location = real_x0,
+            scale = real_a)
+
+# définir fonction log négative pour MLE
+ll = function(location, scale) {
+    dist = suppressWarnings(dcauchy(x,
+                                    location,
+                                    scale,
+                                    log = TRUE))
+    -sum(dist)
+}
+
+# MLE (Maximum Likelihood Estimator)
+library(stats4)
+hat_x0 = median(x)
+d = hist(x,
+         breaks = nbreaks,
+         plot = FALSE) # histogram
+hat_a = 1 / (pi * max(d[["density"]]))
+result = mle(ll,
+             start = list(location = hat_x0, scale = hat_a))
+
+# illustrer
+hist(x,
+     freq = F,
+     breaks = nbreaks,
+     col = "green",
+     xlab = "x",
+     ylab = "Densité",
+     ylim = c(0, 0.2),
+     main = "Loi de Cauchy - réalité vs théorique") # histogramme
+curve(dcauchy(x, real_x0, real_a),
+      add = TRUE,
+      col = "black") # théorique
+curve(dcauchy(x, result@coef[1], result@coef[2]),
+      add = TRUE,
+      col = "red") # réalité
+legend("topright",
+       legend = c("histogramme", "théorique", "réalité"),
+       fill = c("green", "black", "red"))
